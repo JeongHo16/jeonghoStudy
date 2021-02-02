8228623f2a8376591d07cd46054e017e1c9ce8ca
jeongho <cup33c@naver.com>
2021-02-02 22:18:19 +0900

    codebackup

--- codebackup/realTimeMotionTest.lua
+++ codebackup/realTimeMotionTest.lua
@@ -8,7 +8,7 @@ config_jeongho = {
 	"../Resource/jae/social_p1/social_p1.wrl",
 	"../Resource/jae/social_p1/social_p1_copy.wrl.dof",
 	{
---		{'Hips', 'Hips', vector3(0, 0, 0), reversed=false},
+		{'Hips', 'Hips', vector3(0, 0, 0), reversed=false},
 		{'Neck', 'Head', vector3(0, 0, 0), reversed=false},
 		{'LeftElbow', 'LeftWrist', vector3(0, 0, 0), reversed=false},
 		{'RightElbow', 'RightWrist', vector3(0, 0, 0), reversed=false},
@@ -19,6 +19,37 @@ config_jeongho = {
 }
 
 config = config_jeongho
+
+userJointNames = {
+	"JOINT_WAIST",			-- 3
+	"JOINT_TORSO",			-- 2
+	"JOINT_NECK",			-- 1
+	"JOINT_HEAD",			-- 0
+
+	"JOINT_LEFT_COLLAR",	-- 4
+	"JOINT_LEFT_SHOULDER",	-- 5
+	"JOINT_LEFT_ELBOW",		-- 6
+	"JOINT_LEFT_WRIST",		-- 7
+	"JOINT_LEFT_HAND",		-- 8
+	"JOINT_LEFT_FINGERTIP",	-- 9
+
+	"JOINT_RIGHT_COLLAR",	-- 10
+	"JOINT_RIGHT_SHOULDER",	-- 11
+	"JOINT_RIGHT_ELBOW",	-- 12
+	"JOINT_RIGHT_WRIST",	-- 13
+	"JOINT_RIGHT_HAND",		-- 14
+	"JOINT_RIGHT_FINGERTIP",-- 15
+
+	"JOINT_LEFT_HIP",		-- 16
+	"JOINT_LEFT_KNEE",		-- 17
+	"JOINT_LEFT_ANKLE",		-- 18
+	"JOINT_LEFT_FOOT",		-- 19
+
+	"JOINT_RIGHT_HIP",		-- 20
+	"JOINT_RIGHT_KNEE",		-- 21
+	"JOINT_RIGHT_ANKLE",	-- 22
+	"JOINT_RIGHT_FOOT",		-- 23
+}
 
 --[[
 function createIKsolver(loader, config)
@@ -45,7 +76,7 @@ useDevice = true
 tracking = false
 
 function ctor()
-	mEventReceiver=EVR()
+	--mEventReceiver=EVR()
 
 	this:create("Button", "Check Viewpoint", "Check Viewpoint")
 	this:create("Check_Button", "Tracking", "Tracking")
@@ -92,10 +123,14 @@ function ctor()
 --	RE.motionPanel():motionWin():detachSkin(mSkin)
 --	RE.motionPanel():motionWin():addSkin(mSkin)
 
-	mPose = vectorn()
-	mPose:assign(mMotionDOF:row(0))
-	mSkin:setPoseDOF(mPose)
-	mLoader:setPoseDOF(mPose)
+--	mPose = vectorn()
+--	mPose:assign(mMotionDOF:row(0))
+--	mSkin:setPoseDOF(mPose)
+	userPose = Pose()
+	userPose:init(mLoader:numRotJoint(), mLoader:numTransJoint())
+	userPose:identity()	
+	mSkin:_setPose(userPose, mLoader)
+
 --	mPose = Pose()
 --	mLoader:getPose(mPose)
 --	mSkin:_setPose(mPose, mLoader)
@@ -132,7 +167,9 @@ end
 function frameMove(fElapsedTime)
 	if tracking then
 		mNuiListener:waitUpdate()
-		drawUserJoints()
+--		drawUserJoints()
+--		drawLoaderJoints()
+		getUserPose()
 		--conposUpdateFromUser()
 	end
 end
@@ -157,51 +194,86 @@ function onCallback(w, userData)
 	end
 end
 
-function getJointLocalRotFromUser(idx) 
-	if idx<0 then return -1 end
+function getUserPose()
+	local pose = Pose()
+	pose:init(mLoader:numRotJoint(), mLoader:numTransJoint())
+	pose:identity()	
+	local rootTransf = getUserRootTransf()
+	pose:setRootTransformation(rootTransf)
+	local rots = quaterN() rots:pushBack(getJointRotByName("JOINT_WAIST")) 
+	rots:pushBack(getUserJointLocalRot(getJointRotByName("JOINT_WAIST"),getJointRotByName("JOINT_TORSO")))
+	rots:pushBack(quater(1,0,0,0))
+	rots:pushBack(getUserJointLocalRot(getJointRotByName("JOINT_TORSO"),getJointRotByName("JOINT_NECK")))
+	rots:pushBack(getUserJointLocalRot(getJointRotByName("JOINT_NECK"),getJointRotByName("JOINT_HEAD")))
+	
+	--rots:pushBack(quater(1,0,0,0))
+	rots:pushBack(getUserJointLocalRot(getJointRotByName("JOINT_NECK"),getJointRotByName("JOINT_LEFT_COLLAR")))
+	rots:pushBack(getUserJointLocalRot(getJointRotByName("JOINT_LEFT_COLLAR"),getJointRotByName("JOINT_LEFT_SHOULDER")))
+	rots:pushBack(getUserJointLocalRot(getJointRotByName("JOINT_LEFT_SHOULDER"),getJointRotByName("JOINT_LEFT_ELBOW")))
+	rots:pushBack(getUserJointLocalRot(getJointRotByName("JOINT_LEFT_ELBOW"),getJointRotByName("JOINT_LEFT_WRIST")))
 
-	local preRot = quater()
-	local curRot = quater()
-	preRot:getJointRot(idx-1)
-	preRot = preRot:inverse()
-	curRot:getJointRot(idx)
-	return preRot*curRot
+	pose.rotations:assign(rots)
+	userPose = pose	
+	mSkin:_setPose(userPose, mLoader)--setPose안쓴데_setPose쓴데
+end
+
+function getUserRootTransf()--todo: y값 조정
+	local rootRot = getJointRotByName("JOINT_WAIST")
+	local rootPos = getJointPosByName("JOINT_WAIST")
+	return transf(rootRot, rootPos)
+end
+
+function getUserJointLocalRot(preRot, curRot)
+	curRot:toLocal(preRot, curRot)
+	return curRot
 end
 
 function drawUserJoints()
-	print("start")
 	for i=0, 23 do
-		if not(i==9 or i==15 or i==19 or i==23) then
-		--if (i==3 or i==7 or i==13 or i==18 or i==22) then
-		--if (i==0 or i==3 or i==7 or i==13) then
-			--dbg.draw("Sphere", getJointPos(i)+vector3(0,108,0), "ball"..i, "red", 3)
-			--dbg.draw("Axes", transf(getJointRot(i), getJointPos(i)+vector3(0,108,0)), "axesll"..i)
-			print(i)
-			print(getJointRot(i))
+		if not(i==9 or i==15 or i==19 or i==23) then 
+		--if (i==3 or i==7 or i==13 or i==18 or i==22) then 
+			--dbg.namedDraw("Sphere", getJointPos(i)+vector3(0,108,0), "ball"..i, "red", 3)
+			--dbg.namedDraw("Sphere", getJointPosByName(userJointNames[i+1])+vector3(0,108,0), userJointNames[i], "red", 3)
+			dbg.draw("Axes", transf(getJointRot(i), getJointPos(i)+vector3(0,108,0)), "axesll"..i)
 			--dbg.namedDraw("Sphere", getJointPos(i) - getDistance(), i, "red", 3)
-			--dbg.draw("Sphere", getJointPos(i), "ball2l"..i, "blue", 3)
+			dbg.draw("Sphere", getJointPos(i)+vector3(0,108,0), "ball2l"..i, "blue", 3)
 		end
 	end
-	print('end')
 end
 
 function drawLoaderJoints()
 	for i=1, mLoader:numBone()-1 do
-		--dbg.draw("Sphere", mLoader:bone(i):getFrame().translation, mLoader:bone(i):name(), "red", 3)
-		dbg.namedDraw("Axes", mLoader:bone(i):getFrame()*transf(quater(1,0,0,0), vector3(0,108,0)), "axes"..i)
+		dbg.namedDraw("Axes", mLoader:bone(i):getFrame(), "axes"..i)
+		--dbg.namedDraw("Axes", mLoader:bone(i):getFrame()*transf(quater(1,0,0,0), vector3(0,108,0)), "axes"..i)
+		--dbg.namedDraw("Sphere", mLoader:bone(i):getFrame().translation, mLoader:bone(i):name(), "red", 3)
 	end
+end
+
+function getJointPosByName(name)
+	local pos = vector3()
+	pos.x = mNuiListener:getJointPosByName(name,0)/10
+	pos.y = mNuiListener:getJointPosByName(name,1)/10
+	pos.z = mNuiListener:getJointPosByName(name,2)/10
+	return pos
+end
+
+function getJointRotByName(name)
+	local mat = matrix4()
+	local rot = vectorn(9)
+	local quat = quater()
+	for i=0, 8 do
+		rot:set(i, mNuiListener:getJointRotByName(name,i))
+	end
+	mat:setValue(rot(0),rot(1),rot(2),0,rot(3),rot(4),rot(5),0,rot(6),rot(7),rot(8),0,0,0,0,1)
+	quat:setRotation(mat)
+	return quat 
 end
 
 function getJointPos(idx)
 	local pos = vector3()
---	pos.x = -mNuiListener:getJointRealCoords(idx,2)/10
---	pos.y = mNuiListener:getJointRealCoords(idx,1)/10
---	pos.z = -mNuiListener:getJointRealCoords(idx,0)/10
-
-	pos.x = mNuiListener:getJointRealCoords(idx,0)/10
-	pos.y = mNuiListener:getJointRealCoords(idx,1)/10
-	pos.z = mNuiListener:getJointRealCoords(idx,2)/10
-
+	pos.x = mNuiListener:getJointPos(idx,0)/10
+	pos.y = mNuiListener:getJointPos(idx,1)/10
+	pos.z = mNuiListener:getJointPos(idx,2)/10
 	return pos
 end
 
@@ -210,11 +282,14 @@ function getJointRot(idx)
 	local rot = vectorn(9)
 	local quat = quater()
 	for i=0, 8 do
-		rot:set(i, mNuiListener:getJointRotation(idx,i))
+		rot:set(i, mNuiListener:getJointRot(idx,i))
 	end
 	mat:setValue(rot(0),rot(1),rot(2),0,rot(3),rot(4),rot(5),0,rot(6),rot(7),rot(8),0,0,0,0,1)
 	quat:setRotation(mat)
 	return quat 
+end
+
+function dtor()
 end
 
 --[[
@@ -265,7 +340,7 @@ function conposUpdateFromUser()
 	mCON:drawConstraints()
 end
 ]]
-
+--[[
 function handleRendererEvent(ev, button, x,y) 
 	if mCON then
 		return mCON:handleRendererEvent(ev, button, x,y)
@@ -285,9 +360,7 @@ end
 
 function EVR:onFrameChanged(win, iframe)
 end
-
-function dtor()
-end
+]]
 
 --[[
 Timeline=LUAclass(LuaAnimationObject)
